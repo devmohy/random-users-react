2f83efe223a406c5b8decfc29e93980d9c7bd946
Mohammed Yayah <yayahmohammed@gmail.com>
2022-09-20 11:13:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      'users': [],
+      'searchString': '',
+    }
+  }
+
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState(
+      () => {
+        return {'users': users}
+      }
+    ))
+  }
+
+  render(){
+    const filteredUsers = this.state.users.filter((user) => {
+      return user.name.toLowerCase().includes(this.state.searchString)
+    })
+    return (
+      <div className="App">
+        <input type='search' className='search-box' placeholder='search users' onChange={(event) => {
+          console.log({startUsersArray:this.state.users})
+          const searchString = event.target.value.toLowerCase();
+          this.setState(() => {
+            return {searchString: searchString}
+          }, 
+          () => {
+            console.log({endUsersArray:this.state.users})
+          })
+        }} />
+        {
+          filteredUsers.map((user) => {
+            return <div key={user.id}>
+              <h1>{user.name}</h1>
+            </div>
+          })
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
